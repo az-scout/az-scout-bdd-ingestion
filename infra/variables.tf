b33b3cf3b3bf8bb1c8cfc242e5cee8a9d82d3d3b
Rémy Sabile <remy.sabile@microsoft.com>
2026-03-28 12:17:34 +0100

    feat: update currency job timeout to 6 hours for improved processing

--- infra/variables.tf
+++ infra/variables.tf
@@ -143,7 +143,7 @@ variable "currency_start_hour" {
 variable "currency_job_timeout" {
   description = "Replica timeout in seconds for each currency pricing job"
   type        = number
-  default     = 5400 # 1h30
+  default     = 21600 # 6h
 }
 
 # ---------------------------------------------------------------------
